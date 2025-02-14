464a629d0ede431089c536ca19428c8bb69b2694
Heitor Vieira <heitorvieira.pvd@gmail.com>
2025-02-14 09:47:08 -0300

    first commit

---
+++ src/sumNumbers/SumTwoNumbers.java
@@ -0,0 +1,10 @@
+package sumNumbers;
+
+public class SumTwoNumbers {
+	public static int sum(int a, int b) {
+		return a+b;
+	}
+	public static void main(String[] args) {
+		System.out.println(sum(2,3));
+	}
+}
